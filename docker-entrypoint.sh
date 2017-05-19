78de43791189b12dfa5cfd7edc4ac9fc9eba5df2
Mandus Momberg <git@momberg.me>
2017-05-19 16:27:44 -0700

    Added some more control over Redis Endpoints.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/ash
 cd slack-tipbot
-REDIS_URL=redis://redis:${REDIS_CUSTOM_PORT:-6379}/tipbot /usr/local/bundle/bin/rackup config.ru
+REDIS_URL=${REDIS_URL:-redis://redis:${REDIS_CUSTOM_PORT:-6379}/${REDIS_DB_NAME:-tipbot}} /usr/local/bundle/bin/rackup config.ru
+
