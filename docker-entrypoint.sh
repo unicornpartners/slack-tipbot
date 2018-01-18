ac6afcb4c060348b63007d9f8c1dfc4e35a284f7
Mandus Momberg <mandus@mandusmomberg.com>
2018-01-18 09:13:38 -0800

    Fixing the Launch Command to allow for ECS Health Checking

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/ash
-REDIS_URL=${REDIS_URL:-redis://redis:${REDIS_CUSTOM_PORT:-6379}/${REDIS_DB_NAME:-tipbot}} /usr/local/bundle/bin/rackup config.ru
-
+REDIS_URL=${REDIS_URL:-redis://redis:${REDIS_CUSTOM_PORT:-6379}/${REDIS_DB_NAME:-tipbot}} /usr/local/bundle/bin/bundle exec puma -p ${PORT:-8088}
